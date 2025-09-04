53b14593ccb5b71403dbf4b9d00e13ab7f5c4b4a
Marcelo Henrique Barbosa Furlanetto <44544494818@intra.pgfn.gov.br>
2025-09-04 14:14:22 -0300

    Crete filter

---
+++ src/main/java/com/marceloneuro/jwtsecurity/configuration/JwtAuthenticationFilter.java
@@ -0,0 +1,36 @@
+package com.marceloneuro.jwtsecurity.configuration;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
+        final String autheHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String email;
+
+        if(autheHeader == null || !autheHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        jwt = autheHeader.substring(7);
+    }
+}
