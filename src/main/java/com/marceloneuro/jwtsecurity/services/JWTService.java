9fa3ab1677b4e0bb959b297c14014c362369d71e
marcelo-neuro <marcelofurla0813@gmail.com>
2025-09-12 13:51:26 -0300

    Started JWT layer and installed  jwt dependencies

---
+++ src/main/java/com/marceloneuro/jwtsecurity/services/JWTService.java
@@ -0,0 +1,12 @@
+package com.marceloneuro.jwtsecurity.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+    public String extractEmail(String token) {
+
+        return null;
+    }
+}
